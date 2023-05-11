f5b4af574a65c870ee26a1f6f026fffdc593d3b4
Diana Opiyo <dopiyo@gmail.com>
2023-05-11 09:21:19 +0300

    updated routes

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -6,12 +6,12 @@ const Dashboard = require('../models/dashboard');
 
 // @route GET /dashboard
 // @desc Get dashboard data
-router.get('/', (req, res) => {
+router.get('/:id', (req, res) => {
   // Define query object to fetch the dashboard data
-  const query = {};
+  const query = {_id: req.params.id};
 
   // Fetch dashboard data from database using a Promise
-  Dashboard.findOne(query)
+  Dashboard.find(query)
     .then(dashboard => {
       res.json(dashboard);
     })
@@ -23,9 +23,9 @@ router.get('/', (req, res) => {
 
 // @route PUT /dashboard
 // @desc Update dashboard data
-router.put('/', (req, res) => {
+router.get('/:id', (req, res)  => {
   // Define query object to update the dashboard data
-  const query = {};
+  const query = {_id: req.params.id};
 
   // Update dashboard data in the database using a Promise
   Dashboard.findOneAndUpdate(query, req.body, { new: true, upsert: true })
