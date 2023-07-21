40444d10e235a1cf68013210aacd72a1d1e93e63
Mary <ngangamary078@gmail.com>
2023-07-21 21:22:16 +0300

    added some insertions on invoice file

--- models/invoice.js
+++ models/invoice.js
@@ -11,9 +11,11 @@ const invoiceSchema = new mongoose.Schema({
       required: true,
     },
     amount: {
-      type: Number,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product',
       required: true,
     },
+    
   }],
   totalAmount: {
     type: Number,
@@ -22,7 +24,13 @@ const invoiceSchema = new mongoose.Schema({
   date: {
     type: Date,
     default: Date.now,
-  },
+  },  
+  agent: { type: mongoose.Schema.Types.ObjectId,
+    ref: 'SalesAgent', default: '', required: true },
+  shop: { type: mongoose.Schema.Types.ObjectId,
+    ref: 'ShopLoator', default: '', required: true },
+  companyName: { type: String, default: '', required: true },
+  customerName: { type: String, default: '', required: true }
 });
 const Invoice = mongoose.model('Invoice', invoiceSchema);
 
